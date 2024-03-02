a7c1cdee985446d9f06ea8008cda0c97aeb45378
Alexander <TheThirteenthLight@yandex.ru>
2024-03-02 21:40:27 +0300

    smth

--- src/main/java/kur/project0/App.java
+++ src/main/java/kur/project0/App.java
@@ -3,6 +3,9 @@ package kur.project0;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello Wor!");
+
+        System.out.println("Hellor Wor!");
+        System.out.println("lalala");
     }
+
 }
